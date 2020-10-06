c7ce36b1971cb404fa1c6d5a0b5d9b4aa4ea91a0
rmanky <rmankaryous12@gmail.com>
2020-10-06 04:06:32 +0000

    testing in prod

--- src/Hud.svelte
+++ src/Hud.svelte
@@ -19,12 +19,6 @@
     style="flex-basis: 0">
     <HudElement label={'THROTTLE'} value={Math.round($throttle * 100)} />
     <HudElement label={'STEERING'} value={Math.round(-$steering * 50)} />
-    <button
-      class="btn p-3 mb-2 ml-1 mr-1 bg-danger text-light"
-      on:click={() => window.restartRace()}>
-      <h5 class="m-0">RESTART</h5>
-      <h4 class="m-0"><i class="fas fa-redo-alt" /></h4>
-    </button>
   </div>
   <div
     class="d-flex flex-grow-1 flex-shrink-0 justify-content-center align-items-end"
@@ -40,7 +34,12 @@
   <div
     class="d-flex flex-grow-1 flex-shrink-0 justify-content-end mr-1"
     style="flex-basis: 0">
-    <HudElement label={'FPS'} value={$fps} color={'primary'} />
+    <button
+      class="btn p-3 mb-2 ml-1 mr-1 bg-danger text-light"
+      on:click={() => window.restartRace()}>
+      <h5 class="m-0">RESTART</h5>
+      <h4 class="m-0"><i class="fas fa-redo-alt" /></h4>
+    </button>
     <HudElement label={'TIME'} value={$time} />
     <HudElement label={'LAP'} value={$lap + '/2'} />
   </div>
