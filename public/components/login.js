7d06b6afb9b25708d8239cb357d4574d19dfd94a
Connor Burri <cjburri@wpi.edu>
2020-10-07 14:38:09 -0400

    added components for the login screen

---
+++ public/components/login.js
@@ -0,0 +1,9 @@
+class Login extends React.Component {
+    render() {
+        return (
+            <div id="form-header" className="jumbotron">
+                <h1>Please <a href="/auth/github">log in</a> to proceed to your guest list</h1>
+            </div>
+        )
+    }
+}
