0865b2c9224d24c648a17f19f617696a679f0462
Charles Kittler <59940820+cvkittler@users.noreply.github.com>
2020-10-09 23:16:58 -0400

    i dont want a grade i want sleep

---
+++ build/precache-manifest.75e9a6a71acce562f09f9d75d3c6d41e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8762df4ce1fc3563f07b925f5425a691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78616da5498cc7178eca",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "250dfabf5eb0eec460e7",
+    "url": "/static/js/2.88980188.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88980188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78616da5498cc7178eca",
+    "url": "/static/js/main.da17ac7b.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
