ddae397e82a079d0a765de6f75cfc670925707bd
RubyLLL <xlyu00green@gmail.com>
2020-10-10 19:29:00 -0400

    ..

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT ||  5000
 
-app.use(favicon(path.join(__dirname, 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.use(cors())
 app.use(express.json())
