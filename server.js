598d579950fc95e2f2e2f39580821a53a085ec30
mastlouis <mastlouis@wpi.edu>
2020-10-11 16:44:48 -0700

    Inital create-react-app files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express  = require( 'express' ),
+      app      = express(),
+      bp       = require( 'body-parser')
+
+const todos = [
+  { name:'buy groceries', completed:false }
+]
+
+app.use( bp.json() )
+app.use( express.static( 'tagteam-marathon/build' ) )
+
+app.get( '/read', ( req, res ) => res.json( todos ) )
+
+app.post( '/add', ( req,res ) => {
+  todos.push( req.body )
+  res.json( todos )
+})
+
+app.post( '/change', function( req,res ) {
+  const idx = todos.findIndex( v => v.name === req.body.name )
+  todos[ idx ].completed = req.body.completed
+  
+  res.sendStatus( 200 )
+})
+
+app.listen( 8080 )
