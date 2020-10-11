8fff19286618d77e10c040ac6bc14b1b349c10b9
Matthew Gulbin <mjgulbin@wpi.edu>
2020-10-11 19:33:26 -0400

    readme

--- README.md
+++ README.md
@@ -1,30 +1,10 @@
 Assignment 4 - Components
 ===
 
-Due: October 9th, by 11:59 PM.
+## Game High Scores 3.0 (React)
 
-For this assignment you will re-implement the client side portion of A3 using either React or Svelte components.
+https://a4-matthew-gulbin.herokuapp.com/ Matthew Gulbin
 
-This project can be implemented on any hosting service (Glitch, DigitalOcean, Heroku etc.), however, you must include all files in your GitHub repo so that the course staff can view them; these files are not available for viewing in many hosting systems.
+The goal of my application was to create a system that allowed users to sign in via Github and input scores they got in a certain video game, which would be stored in a database. Users can also view, edit, and delete their results from the database using a table on the page.
 
-Deliverables
----
-
-Do the following to complete this assignment:
-
-1. Implement your project with the above requirements.
-3. Test your project to make sure that when someone goes to your main page on Glitch/Heroku/etc., it displays correctly.
-4. Ensure that your project has the proper naming scheme `a4-firstname-lastname` so we can find it.
-5. Fork this repository and modify the README to the specifications below. Be sure to add *all* project files.
-6. Create and submit a Pull Request to the original repo. Name the pull request using the following template: `a4-firstname-lastname`.
-
-Sample Readme (delete the above when you're ready to submit, and modify the below so with your links and descriptions)
----
-
-## Your Web Application Title
-
-your hosting link e.g. http://a4-charlieroberts.glitch.me
-
-Include a very brief summary of your project here and what you changed / added to assignment #3. Briefly (3–4 sentences) answer the following question: did the new technology improve or hinder the development experience?
-
-Unlike previous assignments, this assignment will be solely graded on whether or not you successfully complete it. Partial credit will be generously given.
+For this assignment, I re-implemented the front end of my previous project using React. Overall, I thought that React was significantly easier to use because of the state property. This allowed me to easily keep track of user inputted data. I was also able to implement the edit and delete functionality this time around because onClick listeners are much simpler in React and do not require as much setup.
--- public/script.js
+++ public/script.js
@@ -1,156 +1,156 @@
 "use strict";
 
 class App extends React.Component {
-    constructor(props){
-      super(props)
-      this.state = {
-        yourname: '',
-        score: '',
-        location: '',
-        data: [],
-        error: ''
-      }
-  
-      this.submit = this.submit.bind(this);
-      this.delete = this.delete.bind(this);
-      this.edit = this.edit.bind(this);
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      yourname: '',
+      score: '',
+      location: '',
+      data: [],
+      error: ''
     }
-  
-    componentDidMount() {
-      fetch('/load')
+
+    this.submit = this.submit.bind(this);
+    this.delete = this.delete.bind(this);
+    this.edit = this.edit.bind(this);
+
+  }
+
+  componentDidMount() {
+    fetch('/load')
       .then((res) => res.json())
       .then((json) => {
-        if(!json.error){
-          this.setState({ data: json})
+        if (!json.error) {
+          this.setState({ data: json })
         } else {
           this.setState({ error: json.error })
         }
-        
+
       })
-    }
-  
-    submit(){
-      fetch('/add', {
-        method: "POST",
-        headers: { "Content-Type": "application/json"},
-        body: JSON.stringify({
-          yourname: this.state.yourname,
-          score: this.state.score,
-          location: this.state.location
-        })
+  }
+
+  submit() {
+    fetch('/add', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        yourname: this.state.yourname,
+        score: this.state.score,
+        location: this.state.location
       })
+    })
       .then((res) => res.json())
       .then((json) => {
-        if (!json.error){
+        if (!json.error) {
           this.setState({
             yourname: '',
             score: '',
-            location: '', 
+            location: '',
             data: json,
             error: ''
           })
         } else {
-          this.setState({error : json.error })
+          this.setState({ error: json.error })
         }
-        
+
       })
-    }
-  
-    delete( id ){
-      fetch('/remove', {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ _id: id })
+  }
+
+  delete(id) {
+    fetch('/remove', {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ _id: id })
     })
-    .then((res) => res.json())
-    .then((json) => {
-      if(!json.error){
-        this.setState({ data: json })
-      } else {
-        this.setState({ error: json.error })
-      }
-      })
-    }
-  
-    edit( id ){
-      const dataI = this.state.data.find(i => i._id === id)
-      fetch('/edit', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          _id: dataI._id,
-          yourname: dataI.yourname,
-          score: dataI.score,
-          location: dataI.location
-        })
-      })
       .then((res) => res.json())
       .then((json) => {
-        if(!json.error){
+        if (!json.error) {
           this.setState({ data: json })
         } else {
           this.setState({ error: json.error })
         }
       })
-    }
-  
-    render() {
-      return(
-        <div>
-            <h1 className="heading">Game High Scores!</h1>
-        <a href="/auth/github ">Login with Github</a>
-    <p>Please enter your initials, score, and location here:</p>
-    
-      <input type='text' maxLength="3" id='yourname' defaultValue={this.state.yourname} onChange={e => {this.setState({yourname: e.target.value})}}/>
-      <input type="number" id='score' defaultValue={this.state.score} onChange={e => {this.setState({score: e.target.value})}}/>
-      <input type='text' id='location' defaultValue={this.state.location} onChange={e => {this.setState({location: e.target.value})}}/>
-      <button id="submitbtn" onClick={ this.submit }>Submit</button>
-    
-    <table id='datatable'>
-      <thead>
-        <tr>
-          <th>Initials</th>
-          <th>Score</th>
-          <th>Location</th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody id='scoretable'>
-        {this.state.data.map((i,j) => {
-          return(
-            <tr id={i._id}>
-                <td className='namedata'>
-                  <input type='text' maxlength='3' defaultValue={i.yourname} onChange={e => {i.yourname = e.target.value}}></input>
-                </td>
-                <td className='scoredata'>
-                  <input type='number' defaultValue={i.score} onChange={e => {i.score = e.target.value}}></input>
-                </td>
-                <td className='locationdata'>
-                  <input type='text' defaultValue={i.location} onChange={e => {i.location = e.target.value}}></input>
-                </td>
-                <td>
-                <button onClick={() => this.delete(i._id)}>Delete</button>
-                </td>
-                <td>
-                <button onClick={() => this.edit(i._id)}>Edit</button>
-                </td>
-              </tr>
-          )
-        }
-        )}
-      </tbody>
-    </table>
-        </div>
-        
-      )
-    }
   }
 
-  const mountNode = document.querySelector("#root");
-  ReactDOM.render(<App/>, mountNode);
+  edit(id) {
+    const dataI = this.state.data.find(i => i._id === id)
+    fetch('/edit', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        _id: dataI._id,
+        yourname: dataI.yourname,
+        score: dataI.score,
+        location: dataI.location
+      })
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (!json.error) {
+          this.setState({ data: json })
+        } else {
+          this.setState({ error: json.error })
+        }
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="heading">Game High Scores!</h1>
+        <a href="/auth/github ">Login with Github</a>
+        <p>Please enter your initials, score, and location here:</p>
+
+        <input type='text' maxLength="3" id='yourname' defaultValue={this.state.yourname} onChange={e => { this.setState({ yourname: e.target.value }) }} />
+        <input type="number" id='score' defaultValue={this.state.score} onChange={e => { this.setState({ score: e.target.value }) }} />
+        <input type='text' id='location' defaultValue={this.state.location} onChange={e => { this.setState({ location: e.target.value }) }} />
+        <button id="submitbtn" onClick={this.submit}>Submit</button>
+
+        <table id='datatable'>
+          <thead>
+            <tr>
+              <th>Initials</th>
+              <th>Score</th>
+              <th>Location</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody id='scoretable'>
+            {this.state.data.map((item, index) => {
+              return (
+                <tr id={item._id}>
+                  <td className='namedata'>
+                    <input type='text' maxlength='3' defaultValue={item.yourname} onChange={e => { item.yourname = e.target.value }}></input>
+                  </td>
+                  <td className='scoredata'>
+                    <input type='number' defaultValue={item.score} onChange={e => { item.score = e.target.value }}></input>
+                  </td>
+                  <td className='locationdata'>
+                    <input type='text' defaultValue={item.location} onChange={e => { item.location = e.target.value }}></input>
+                  </td>
+                  <td>
+                    <button onClick={() => this.delete(item._id)}>Delete</button>
+                  </td>
+                  <td>
+                    <button onClick={() => this.edit(item._id)}>Edit</button>
+                  </td>
+                </tr>
+              )
+            }
+            )}
+          </tbody>
+        </table>
+      </div>
+
+    )
+  }
+}
+
+const mountNode = document.querySelector("#root");
+ReactDOM.render(<App />, mountNode);
