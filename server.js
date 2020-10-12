2b45ed2baa283d27442ecd19925a3eedf1ddd7ee
mastlouis <mastlouis@wpi.edu>
2020-10-11 20:09:17 -0700

    Add, view notes, no edit or delete

--- server.js
+++ server.js
@@ -1,26 +1,156 @@
-const express  = require( 'express' ),
-      app      = express(),
-      bp       = require( 'body-parser')
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const passport = require('passport');
+const GitHubStrategy = require('passport-github').Strategy;
+const app = express();
+const auth = require('connect-ensure-login');
+const timeout = require('connect-timeout');
 
-const todos = [
-  { name:'buy groceries', completed:false }
-]
+const MongoDB = require('mongodb');
+const MongoClient = MongoDB.MongoClient;
+const uri = `mongodb+srv://gratitude-robot:${ process.env.DBPASS }@a3-primary.4sekk.mongodb.net/${ process.env.DBNAME }?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+let collection = null;
+client.connect(err => {
+  collection = client.db("test").collection("devices");
+  console.log(collection);
+});
 
-app.use( bp.json() )
-app.use( express.static( 'tagteam-marathon/build' ) )
+app.use(timeout('5s'));
+function haltOnTimedout (req, res, next) {
+  if (!req.timedout) next()
+}
 
-app.get( '/read', ( req, res ) => res.json( todos ) )
+app.use(express.static("tagteam-marathon/build"));
+app.use(haltOnTimedout);
 
-app.post( '/add', ( req,res ) => {
-  todos.push( req.body )
-  res.json( todos )
-})
+app.use(require('morgan')('combined'));
+app.use(haltOnTimedout);
+app.use(require('cookie-parser')());
+app.use(haltOnTimedout);
+app.use(require('express-session')({ secret: 'hyperbolic paraboloid', resave: true, saveUninitialized: true }));
+app.use(haltOnTimedout);
+app.use(passport.initialize());
+app.use(haltOnTimedout);
+app.use(passport.session());
+app.use(haltOnTimedout);
 
-app.post( '/change', function( req,res ) {
-  const idx = todos.findIndex( v => v.name === req.body.name )
-  todos[ idx ].completed = req.body.completed
-  
-  res.sendStatus( 200 )
-})
+app.use(require("express-lowercase-paths")());
+app.use(haltOnTimedout);
+app.use(require('serve-favicon')(path.join(__dirname, 'tagteam-marathon/assets', 'favicon.png')));
+app.use(haltOnTimedout);
+app.use(require('helmet')({contentSecurityPolicy: false}));
+app.use(haltOnTimedout);
+app.use(require("connect-rid")({headerName: "x_rid"}));
+app.use(haltOnTimedout);
 
-app.listen( 8080 )
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+// ----------
+// Middleware
+// ----------
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK
+},
+function(accessToken, refreshToken, profile, cb) {
+  // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+  //   return cb(err, user);
+  // });
+  return cb(null, profile);
+}
+));
+app.use(haltOnTimedout);
+
+app.get('/auth/github', 
+  passport.authenticate('github', { successReturnToOrRedirect: '/', failureRedirect: '/', failureFlash: 'Authentication Failed'}));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+});
+
+// ----------------------
+// Requests and Responses
+// ----------------------
+
+app.get('/get-runs', auth.ensureLoggedIn('/auth/github'), function getRuns(request, response){
+  const cursor = collection.find({"user": request.user.username}) // get everything
+  cursor.toArray().then(array => {
+    console.log(`Array data: ${JSON.stringify(array)}`);
+    response.json(array);
+  });
+});
+
+app.post('/add-run', bodyParser.json(), auth.ensureLoggedIn('/auth/github'), function addRun (request, response) {
+  let runToAdd = request.body;
+  runToAdd.user = request.user.username;
+  console.log(`Adding run ${JSON.stringify(runToAdd)}`);
+  collection.insertOne(request.body)
+  .then(dbresponse => {
+    console.log(`dbresponse: ${dbresponse}`);
+    response.json( dbresponse.ops[0] );
+  });
+});
+
+app.post('/delete-run', bodyParser.json(), auth.ensureLoggedIn('/auth/github'), function deleteRun (request, response) {
+  console.log(`ID to delete :${JSON.stringify(request.body.id)}`);
+  collection.deleteOne({ _id:MongoDB.ObjectID( request.body.id ) })
+    .then( result => response.json(result) );
+});
+
+app.post('/edit-run', bodyParser.json(), auth.ensureLoggedIn('/auth/github'), function editRun (request, response) {
+  let runToEdit = request.body.run;
+  runToEdit.user = request.user.username;
+  collection.update({_id:MongoDB.ObjectID(request.body.id)}, runToEdit)
+    .then( result => response.json(result));
+});
+
+app.get('/user-existence', auth.ensureLoggedIn('/auth/github'), function userExistence (request, response) {
+  //If there is no user
+  if(!request.user.username) {
+    res.json('none');
+  }
+
+  console.log(`Checking for user :${request.user.username}`);
+  const cursor = collection.find({"username": request.user.username}) // get everything
+  cursor.toArray().then(array => {
+    console.log(`Array data: ${JSON.stringify(array)}`);
+
+    // If no such user exists
+    if(!array.length) {
+      console.log(`Adding user ${request.user.username}`);
+      collection.insertOne({username: request.user.username, new: false})
+      .then(dbresponse => {
+        console.log(`dbresponse: ${dbresponse}`);
+        response.json({userState: 'false', username: request.user.username});
+      });
+    }
+    // If such a user exists
+    else {
+      response.json({userState: 'true', username: request.user.username});
+    }
+
+  });
+});
--- tagteam-marathon/src/App.js
+++ tagteam-marathon/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -24,6 +24,9 @@ function NavBar() {
 }
 
 function MainDisplay() {
+  let [isLoggedIn, changeLoggedIn] = useState(false)
+  let [appData, setAppData] = useState(null);
+
   return (
     <main role="main">
       <div class="jumbotron">
@@ -37,15 +40,44 @@ function MainDisplay() {
           </p>
         </div>
       </div>
-      <LoginOrContent />
+      <LoginOrContent appData={appData} isLoggedIn={isLoggedIn} changeLoggedIn={changeLoggedIn} setAppData={setAppData} />
     </main>
   );
 }
 
-function LoginOrContent() {
-  let loggedIn = false;
+function LoginOrContent(props) {
+  function checkUser() {
+    fetch('/user-existence', {
+        method: 'GET',
+    })
+    .then(response => response.json())
+    .then(function( response ){
+        if(response.userState === 'false') {
+            console.log(`Created new user ${response.username}`);
+            alert(`Creating new user ${response.username}. Welcome to Tagteam Marathon!`);
+        } // If such a user already exists, exit silently
+    });
+  }
 
-  if (loggedIn) {
+  const loadData = function() {
+    fetch('/get-runs', {
+        method: 'GET',
+    })
+    .then(response => response.json())
+    .then(parsedData => {
+        if (!props.isLoggedIn){
+          props.setAppData(parsedData);
+          props.changeLoggedIn(!!parsedData);
+        }
+    });
+  }
+
+  useEffect(() => {
+    checkUser();
+    loadData();
+  })
+
+  if (!props.isLoggedIn) {
     return (
       <Login />
     );
@@ -53,8 +85,8 @@ function LoginOrContent() {
 
   return (
     <div class="container" id="user-content">
-      <CompletedRuns />
-      <NewRunForm />
+      <CompletedRuns runs={props.appData} changeLoggedIn={props.changeLoggedIn}/>
+      <NewRunForm changeLoggedIn={props.changeLoggedIn}/>
     </div>
   );
 }
@@ -68,7 +100,8 @@ function Login() {
   );
 }
 
-function CompletedRuns() {
+function CompletedRuns(props) {
+  let totalDist = 0;
   return (
     <>
       <h2>Completed Runs</h2>
@@ -82,6 +115,15 @@ function CompletedRuns() {
             <th>Time (minutes)</th>
             <th>Speed (miles/hour)</th>
           </tr>
+          { 
+            props.runs.map( (run, i) => {
+              totalDist += parseInt(run.distance);
+              if (true) {
+                return ( <CompletedRunEntry run={run} index={i} changeLoggedIn={props.changeLoggedIn}/> );
+              }
+              return ( <EditableRunEntry run={run} index={i} changeLoggedIn={props.changeLoggedIn}/>);
+            })
+          }
         </table>
       </div>
       <div class="table-responsive">
@@ -91,8 +133,8 @@ function CompletedRuns() {
             <th>Marathons Run</th> 
           </tr>
           <tr>
-            <td id="total-distance"></td>
-            <td id="num-marathons"></td>
+            <td>{ totalDist }</td>
+            <td>{ (totalDist / 26.2).toFixed(2) }</td>
           </tr>
         </table>
       </div>
@@ -100,7 +142,68 @@ function CompletedRuns() {
   );
 }
 
-function NewRunForm() {
+function CompletedRunEntry(props) {
+  return (
+    <tr>
+      <td>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu-${i}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Actions
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <button class="dropdown-item" type="button" onclick="deleteRun('${data[i]._id}', ${i})">Delete Run</button>
+                <button class="dropdown-item" type="button" onclick="prepareEdit(${i + 1}, '${data[i]._id}')">Edit Run</button>
+                <button class="dropdown-item" type="button" onClick={
+                  () => alert(props.run.notes ? props.run.notes : 'No notes exist for this run')
+                  }>View Notes</button>
+                <button class="dropdown-item" type="button" onClick={
+                  () => {
+                    let newNotes = prompt("Edit Run Notes", props.run.notes ? props.run.notes : "");
+                    if(newNotes) {
+                      let runToSend = {...props.run};
+                      runToSend.notes = newNotes;
+                      
+                      fetch('/edit-run', {
+                          method: 'POST',
+                          body: JSON.stringify({run: runToSend, id: props.run._id}),
+                          headers: {
+                              "Content-Type": "application/json"
+                          }
+                      }).then(function handleEditRunResponse(response) {
+                          if (response.status === 200) {  // OK
+                              console.log(`Successfully edited run to ${JSON.stringify(runToSend)}`);
+                              props.changeLoggedIn(false);  // Refresh the tables
+                          } else {
+                              console.error(`Failed to edit run to ${JSON.stringify(runToSend)}
+                              Error: ${response.message}`);
+                          }
+                      });
+                    }
+                  }
+                }>Edit Notes</button>
+            </div>
+        </div>
+      </td>
+      <td>{ props.run.name }</td>
+      <td>{ props.run.location }</td>
+      <td>{ props.run.distance }</td>
+      <td>{ props.run.time }</td>
+      <td>{ (props.run.distance * 60 / props.run.time).toFixed(2) }</td>
+    </tr>
+  );
+}
+
+function EditableRunEntry(props) {
+
+}
+
+function NewRunForm(props) {
+  let [name, setName] = useState("");
+  let [location, setLocation] = useState("");
+  let [distance, setDistance] = useState(0);
+  let [time, setTime] = useState(0);
+  let [notes, setNotes] = useState("");
+
   return (
     <div class="container">
       <h2>New Run</h2>
@@ -110,21 +213,21 @@ function NewRunForm() {
           <div class="input-group-prepend">
             <label class="input-group-text" for="input-name">Your Name</label>
           </div>
-          <input class="form-control" type="text" id="input-name" placeholder="ex. Jane Doe" />
+          <input class="form-control" type="text" id="input-name" placeholder="ex. Jane Doe" value={name} onChange={(e)=>setName(e.target.value)} />
         </div>
         
         <div class="input-group mb-3">
           <div class="input-group-prepend">
             <label class="input-group-text" for="input-location">Location</label>
           </div>
-          <input class="form-control" type="text" id="input-location" placeholder="ex. Bancroft Tower" />
+          <input class="form-control" type="text" id="input-location" placeholder="ex. Bancroft Tower" value={location} onChange={(e)=>setLocation(e.target.value)} />
         </div>
         
         <div class="input-group mb-3">
           <div class="input-group-prepend">
             <label class="input-group-text" for="input-distance"> Distance </label>
           </div>
-          <input class="form-control" type="number" id="input-distance" placeholder="ex. 2.75" min="0" step="0.25" />
+          <input class="form-control" type="number" id="input-distance" placeholder="ex. 2.75" min="0" step="0.25" value={distance} onChange={(e)=>setDistance(e.target.value)} />
           <div class="input-group-append">
             <span class="input-group-text">miles</span>
           </div>
@@ -134,7 +237,7 @@ function NewRunForm() {
           <div class="input-group-prepend">
             <label class="input-group-text" for="input-time"> Time </label>
           </div>
-          <input class="form-control" type="number" id="input-time" placeholder="ex. 30" min="0" />
+          <input class="form-control" type="number" id="input-time" placeholder="ex. 30" min="0" value={time} onChange={(e)=>setTime(e.target.value)} />
           <div class="input-group-append">
             <span class="input-group-text">minutes</span>
           </div>
@@ -142,10 +245,37 @@ function NewRunForm() {
 
         <div class="form-group">
           <label for="input-notes">Notes</label>
-          <textarea class="form-control" id="input-notes" rows="3"></textarea>
+          <textarea class="form-control" id="input-notes" rows="3" value={notes} onChange={(e)=>setNotes(e.target.value)}></textarea>
         </div>
         
-        <button class="btn btn-primary" id="form-submit" type="submit">Submit Run</button>
+        <button class="btn btn-primary" id="form-submit" type="submit" onClick={
+          () => {
+            const body = {
+                name: name,
+                location: location,
+                distance: distance,
+                time: time,
+                notes: notes,
+            };
+
+            fetch('/add-run', {
+              method: 'POST',
+              body: JSON.stringify(body),
+              headers: {
+                "Content-Type": "application/json"
+              }
+            }).then(function handleAddRunResponse(response) {
+              if (response.status === 200) {  // OK
+                console.log(`Successfully added run ${JSON.stringify(body)}`);
+                props.changeLoggedIn(false);
+              } else {
+                console.error(`Failed to add run ${JSON.stringify(body)};
+                Error: ${response.message}`);
+                debugger;
+              }
+            });
+          }
+        }>Submit Run</button>
       </form>
     </div>
   );
