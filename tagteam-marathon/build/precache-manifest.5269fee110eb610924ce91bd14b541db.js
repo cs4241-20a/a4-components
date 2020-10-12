d4a61e3adbd6dee0d5dd2823300da6dd83caede8
mastlouis <mastlouis@wpi.edu>
2020-10-11 20:09:55 -0700

    Have some build files

---
+++ tagteam-marathon/build/precache-manifest.5269fee110eb610924ce91bd14b541db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc8ee77c044b73bd143cf613491338b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1374ccec192355c05dea",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "31aef63925a78f5907e9",
+    "url": "/static/js/2.f0193733.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f0193733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1374ccec192355c05dea",
+    "url": "/static/js/main.9a9cc939.chunk.js"
+  },
+  {
+    "revision": "6e0d417c13b9a74ff3a7",
+    "url": "/static/js/runtime-main.d0320da4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
