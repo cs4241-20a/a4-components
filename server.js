4054404b570553c519fed0fe5db0b382c52bfd3f
mmtolbert <mmtolbert@cox.net>
2020-10-11 23:41:14 -0400

    Removed node_modules and finished assignment

--- server.js
+++ server.js
@@ -4,86 +4,105 @@
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
-const fetch = require("node-fetch")
-const compression = require("compression")
-const cookieSession = require("cookie-session")
+const fetch = require("node-fetch");
+const compression = require("compression");
+const cookieSession = require("cookie-session");
 const app = express();
-const bodyparse = require('body-parser')
-require('dotenv').config()
+const bodyparse = require("body-parser");
+require("dotenv").config();
 
 // middleware #1 - // make all the files in 'public' available
 app.use(express.static("public"));
 // middleware #2 - cookie session
-app.use(cookieSession({
-  secret: process.env.COOKIE_SECRET
-}))
+app.use(
+  cookieSession({
+    secret: process.env.COOKIE_SECRET
+  })
+);
 
-const mongodb = require('mongodb')
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
 const uri = `mongodb+srv://admin:a3-admin-password@a3-matt-tolbert.gv63o.mongodb.net/a4?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
-let collection = null
+let collection = null;
 client.connect(err => {
   collection = client.db("a4").collection("database");
 });
 
 // middleware #3 - verify mongodb connection
-app.use( (req,res,next) => {
-  if( collection !== null ) {
-    next()
-  }else{
-    res.status( 503 ).send()
+app.use((req, res, next) => {
+  if (collection !== null) {
+    next();
+  } else {
+    res.status(503).send();
   }
-})
+});
 
 // middleware #4 - convert body to json using bodyparser (consolidates parsing of body for add/modify/delete)
-app.use(bodyparse.json())
+app.use(bodyparse.json());
 
 // middleware #5 - use compression
-app.use(compression({ filter: shouldCompress }))
+app.use(compression({ filter: shouldCompress }));
 
-function shouldCompress (req, res) {
-  if (req.headers['x-no-compression']) {
+function shouldCompress(req, res) {
+  if (req.headers["x-no-compression"]) {
     // don't compress responses with this request header
-    return false
+    return false;
   }
 
   // fallback to standard filter function
-  return compression.filter(req, res)
+  return compression.filter(req, res);
 }
 
-app.get("/", (_, response) => {
-    response.sendFile(__dirname + "/public/index.html");
+app.get("/", (request, response) => {
+  const { html } = app.render({ url: request.url });
+
+  app.render({
+    url: request.url
+  });
 });
 
 /* 
 Handle OAuth with GitHub - Tutorial from Kevin Simper @ 
 https://www.kevinsimper.dk/posts/how-to-make-authentication-with-github-apps-for-side-projects 
 */
-const clientID = 'c2008bf2f554f3cee63a'
-const clientSecret = '9074c568dd61cdc5650c585f927f23ece30a9fcf'
+const clientID = "c2008bf2f554f3cee63a";
+const clientSecret = "9074c568dd61cdc5650c585f927f23ece30a9fcf";
 
-app.get('/login', (req, res) => {
-  const path = req.protocol + '://' + req.get('host');
+app.get("/checkLogin", (req, res) => {
+  if (req.session.userID) {
+    console.log("user was logged in...")
+    const json = {"loggedIn": "true"};
+    res.json(json);
+  }
+  else {
+    console.log("user was not logged in...")
+    const json = {"loggedIn": "false"};
+    res.json(json);
+  }
+})
+
+app.get("/login", (req, res) => {
+  const path = req.protocol + "://" + req.get("host");
   const url = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${path}/login/github/callback`;
   res.json(url);
-})
+});
 
 // get access token from github oauth
 async function getAccessToken(code, client_id, client_secret) {
-  const request = await fetch('https://github.com/login/oauth/access_token', {
-    method: 'POST',
+  const request = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
       client_id,
       client_secret,
-      code,
-    }),
-  })
+      code
+    })
+  });
   const text = await request.text();
   const params = new URLSearchParams(text);
   return params.get("access_token");
@@ -91,89 +110,108 @@ async function getAccessToken(code, client_id, client_secret) {
 
 // get users profile from GitHub API
 async function fetchGitHubUser(token) {
-  const request = await fetch('https://api.github.com/user', {
+  const request = await fetch("https://api.github.com/user", {
     headers: {
-      Authorization: 'token ' + token,
-    },
-  })
-  return await request.json()
+      Authorization: "token " + token
+    }
+  });
+  return await request.json();
 }
 
-app.get('/login/github/callback', async (req, res) => {
-  const code = req.query.code
-  const access_token = await getAccessToken(code, clientID, clientSecret)
-  const user = await fetchGitHubUser(access_token)
-  if(user) {
-    console.log("user logged in as " + user.id)
+app.get("/login/github/callback", async (req, res) => {
+  const code = req.query.code;
+  const access_token = await getAccessToken(code, clientID, clientSecret);
+  const user = await fetchGitHubUser(access_token);
+  if (user) {
+    console.log("user logged in as " + user.id);
     req.session.userID = user.id;
-    res.redirect("/")
-  }
-  else {
+    res.redirect("/");
+  } else {
     // reload the page
-    console.log("Could not login user")
-    res.redirect("/login.html")
+    console.log("Could not login user");
+    res.redirect("/login.html");
   }
-})
+});
 
 // logout user
-app.get('/logout', (req, res) => {
-  if (req.session) req.session = null
-  res.redirect('/')
-})
-
+app.get("/logout", (req, res) => {
+  console.log("user logged out")
+  if (req.session) req.session = null;
+  res.redirect("/");
+});
 
 /*
 Data Management endpoints
 */
-app.get('/golfbag', (req, res) => {
+app.get("/golfbag", (req, res) => {
   var userData = [];
-  collection.find({"userID": req.session.userID}).toArray().then( result => res.json( result ) )
-})
+  collection
+    .find({ userID: req.session.userID })
+    .toArray()
+    .then(result => res.json(result));
+});
 
-app.post('/add', (req, res) => {
+app.post("/golfbagItem", (req, res) => {
+  collection
+    .find({ _id: mongodb.ObjectID(req.body._id) })
+    .toArray()
+    .then(result => res.json(result));
+});
+
+app.post("/add", (req, res) => {
   // pull contents from req and insert to DB
   const contents = {
-    userID:         req.session.userID,
-    manufacturer:   req.body.manufacturer,
-    model:          req.body.model,
-    type:           req.body.type,
-    loft:           req.body.loft,
-    distance:       req.body.distance,
-    ballSpeed:      req.body.ballSpeed,
-    swingSpeed:     req.body.swingSpeed,
-  }
-  console.log(contents)
-  collection.insertOne( contents )
-  .then(result => {
-    // return entry from DB so that client can keep track of _id
-    res.json(result.ops[0])
-  })
-})
-
-app.post( '/delete', (req, res) => {
+    userID: req.session.userID,
+    manufacturer: req.body.manufacturer,
+    model: req.body.model,
+    type: req.body.type,
+    loft: req.body.loft,
+    distance: req.body.distance,
+    ballSpeed: req.body.ballSpeed,
+    swingSpeed: req.body.swingSpeed
+  };
+  console.log(contents);
   collection
-    .deleteOne( { _id:mongodb.ObjectID( req.body._id ) } ) 
-    .then(result => res.json(result))
-})
+    .insertOne(contents)
+    .then(
+      collection
+        .find({ userID: req.session.userID })
+        .toArray()
+        .then(result => res.json(result))
+    );
+});
 
-app.post( '/update', (req, res) => {
+app.post("/delete", (req, res) => {
+  collection
+    .deleteOne({ _id: mongodb.ObjectID(req.body._id) })
+    .then(
+      collection
+        .find({ userID: req.session.userID })
+        .toArray()
+        .then(result => res.json(result))
+    );
+});
+
+app.post("/update", (req, res) => {
   const json = {
-    userID:         req.session.userID,
-    manufacturer:   req.body.manufacturer,
-    model:          req.body.model,
-    type:           req.body.type,
-    loft:           req.body.loft,
-    distance:       req.body.distance,
-    ballSpeed:      req.body.ballSpeed,
-    swingSpeed:     req.body.swingSpeed,
+    userID: req.session.userID,
+    manufacturer: req.body.manufacturer,
+    model: req.body.model,
+    type: req.body.type,
+    loft: req.body.loft,
+    distance: req.body.distance,
+    ballSpeed: req.body.ballSpeed,
+    swingSpeed: req.body.swingSpeed
   };
   collection
-    .updateOne(
-      { _id:mongodb.ObjectID( req.body._id ) }, 
-      { $set: json }
-    )
-  .then( result => res.json(result))
-})
+    .updateOne({ _id: mongodb.ObjectID(req.body._id) }, { $set: json })
+    .then(
+      collection
+        .find({ userID: req.session.userID })
+        .toArray()
+        .then(result => res.json(result))
+    );
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,391 @@
-// to be completed
+<script>
+  // figure out what to display
+  var loggedIn = false;
+  
+  window.onload = function() {
+     // check if logged in
+    console.log("the window on load ran")
+    fetch('/checkLogin', {
+          method:'GET'
+      })
+      .then(function(response){
+        return response.json(); 
+      })
+      .then( json => {
+          console.log(json)
+          if (json.loggedIn === "true") {
+            loggedIn = true;
+          }
+          else {
+            // hide login and show table
+            console.log("user not logged in")
+            document.getElementById("login").hidden = false;  
+          }
+    })
+    
+    return false;
+  }
+  
+  $: if (loggedIn) {
+    // hide login and show table
+    console.log("user logged in")
+    document.getElementById("login").hidden = true;
+    document.getElementById("main").hidden = false;
+  }
+  
+  // handle login
+  const performLogin = function( e ) {
+      e.preventDefault();
+
+      fetch('/login', {
+          method:'GET'
+      })
+      .then( response => response.json() )
+      .then( url => {
+          // check if logged in properly
+          fetch('/checkLogin', {
+                method:'GET'
+            })
+            .then( response => response.text() )
+            .then( text => {
+                if (text === "logged in") {
+                  loggedIn = true;
+                }
+          })
+        window.location.href=url
+      })
+
+      return false;
+  }
+  // handle logout
+  const logout = function(e) {
+    e.preventDefault();
+
+    fetch("/logout", {
+      method: "GET"
+    }).then(url => {
+      window.location.href = "/";
+    });
+    
+    loggedIn = false;
+    
+    // show login screen
+    console.log("user not logged in")
+    document.getElementById("login").hidden = false;
+    document.getElementById("main").hidden = true;
+
+    return false;
+  };
+  
+  
+  // handle everything else
+  const clear = function(e) {
+    e.preventDefault();
+
+    const manufacturer = document.querySelector("#clubManufacturer"),
+      model = document.querySelector("#clubModel"),
+      type = document.querySelector("#clubType"),
+      loft = document.querySelector("#clubLoft"),
+      distance = document.querySelector("#clubDistance");
+
+    manufacturer.value = "";
+    model.value = "";
+    type.value = "";
+    loft.value = "";
+    distance.value = "";
+
+    // reset buttons
+    const button = document.querySelector("#addButton");
+    button.innerHTML = "Submit";
+    button.onclick = submit;
+
+    const clearButton = document.querySelector("#clearButton");
+    clearButton.innerHTML = "Clear";
+    clearButton.onclick = clear;
+  };
+
+  const retrieveInput = function() {
+    console.log("Some version of submit hit");
+    // create json listing of input params
+    const manufacturer = document.querySelector("#clubManufacturer"),
+      model = document.querySelector("#clubModel"),
+      type = document.querySelector("#clubType"),
+      loft = document.querySelector("#clubLoft"),
+      distance = document.querySelector("#clubDistance"),
+      json = {
+        manufacturer: manufacturer.value,
+        model: model.value,
+        type: type.value,
+        loft: Number(loft.value).toFixed(1),
+        distance: Number(distance.value).toFixed(1),
+        ballSpeed: (distance.value / 1.75).toFixed(1),
+        swingSpeed: (distance.value / 1.75 / 1.5).toFixed(1)
+      };
+
+    // check for empty values
+    for (const [key, value] of Object.entries(json)) {
+      if (typeof value === 'string' && value === "") {
+        console.warn("Must specify value for " + key + "!");
+        return false;
+      }
+    }
+
+
+    // verify distance and loft are numbers
+    if (isNaN(loft.value)) {
+      console.warn("Loft must be a number!");
+      return false;
+    }
+    if (isNaN(distance.value)) {
+      console.warn("Distance must be a number!");
+      return false;
+    }
+
+    return JSON.stringify(json);
+  };
+
+  const submit = function(e) {
+    // prevent default form action from being carried out
+    e.preventDefault();
+
+    // retrieve input
+    const json = retrieveInput();
+
+    // verify json is valid
+    if (!json) {
+      // don't clear invalid requests
+      return false;
+    }
+
+    // add to server and update promise
+    promise = fetch("/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: json
+    })
+    .then(response => response.json());
+
+    // clear inputs now that we have retrieved them
+    clear(e);
+    return false;
+  };
+
+  const submitEdits = function(dataID) {
+    // retrieve input
+    const json = retrieveInput();
+
+    // verify json is valid
+    if (!json) {
+      return;
+    }
+
+    var object = JSON.parse(json);
+    object._id = dataID;
+    const body = JSON.stringify(object)
+
+    console.log("trying to post edit")
+    
+    // add to server and update promise
+    promise = fetch("/update", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: body
+    }).then(response => response.json());
+
+    return false;
+  };
+  
+  const edit = function(dataID) {  
+    var element = null;
+    
+    // get fields
+    fetch("/golfbagItem", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({_id: dataID})
+    }).then(function(response) {
+        return response.json(); // wait on response
+    }).then(function(array) {
+        console.log(array)
+        element = array[0];
+      
+        const manufacturer = document.querySelector("#clubManufacturer"),
+          model = document.querySelector("#clubModel"),
+          type = document.querySelector("#clubType"),
+          loft = document.querySelector("#clubLoft"),
+          distance = document.querySelector("#clubDistance");
+
+        manufacturer.value = element.manufacturer;
+        model.value = element.model;
+        type.value = element.type;
+        loft.value = element.loft;
+        distance.value = element.distance;
+
+        // change submit button to become "update field"
+        const button = document.querySelector("#addButton");
+        button.innerHTML = "Update";
+        button.onclick = function(e) {
+          e.preventDefault(); //prevent reload
+          submitEdits(dataID); //submit edits
+          clear(e); //clear the inputs and reset buttons
+        };
+
+        const clearButton = document.querySelector("#clearButton");
+        clearButton.innerHTML = "Cancel";
+    });
+
+    return false;
+  };
+  
+  const deleteFunc = function(dataID) {
+    // remove entry from database
+    promise = fetch("/delete", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ _id: dataID })
+    }).then(response => response.json());
+
+    // now that item is deleted, it can't be edited
+    // handle weird edge case of deleting item while editing it
+    const clearButton = document.querySelector("#clearButton");
+    if (clearButton.innerHTML === "Cancel") {
+      clearButton.click();
+    }
+
+    return false;
+  };
+  
+    const getGolfBag = function() {
+      const p = fetch( '/golfBag', {
+        method:'GET' 
+      })
+      .then( response => response.json() )
+      .then( json => {
+        console.log(json)
+        return json 
+      })
+   
+      return p
+    }
+  
+    const clearInput = function( e ) {
+      e.preventDefault();
+  
+      const manufacturer = document.querySelector("#clubManufacturer"),
+        model = document.querySelector("#clubModel"),
+        type = document.querySelector("#clubType"),
+        loft = document.querySelector("#clubLoft"),
+        distance = document.querySelector("#clubDistance");
+  
+      manufacturer.value = "";
+      model.value = "";
+      type.value = "";
+      loft.value = "";
+      distance.value = "";
+  
+      // reset buttons
+      const button = document.querySelector("#addButton");
+      button.innerHTML = "Submit";
+      button.onclick = submit;
+  
+      const clearButton = document.querySelector("#clearButton");
+      clearButton.innerHTML = "Clear";
+      clearButton.onclick = clearInput;
+    }
+  
+    let promise = getGolfBag()
+  </script>
+  
+<div id="login" hidden="true">
+  <head>
+      <title>Golf Club Tracker - Login</title>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/yegor256/tacit@gh-pages/tacit-css-1.5.1.min.css"/>
+      <script src="login.js" defer></script>
+  </head>
+
+  <body>
+      <section>
+          <div class="centeredItem">
+              <div style="text-align:center;" id="content-wrapper">
+                  <h1 style="font-size:400%;" id="loginTitle">Golf Club Tracker</h1>
+                  <br>
+                  <button on:click={performLogin} style="font-size:200%; color:blue;" id="loginButton">Login via GitHub</button>
+              </div>
+          </div>
+      </section>
+  </body>
+</div>
+
+
+<div id="main" hidden="true">
+  <head>
+    <title>Golf Club Tracker</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/yegor256/tacit@gh-pages/tacit-css-1.5.1.min.css"/>
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <div style="text-align:center;">
+        <h1>Golf Club Tracker</h1>
+        <h2>
+          Keep track of your clubs! Free estimates of your ball & swing speed!
+        </h2>
+        <button on:click={logout} style="position:absolute;top:0;right:0;" id="logoutButton">Logout</button>
+      </div>
+      <br>
+  
+    <div class="inputForm">
+      <form style="text-align:center;"  id="clubForm">
+        <input type="text" id="clubManufacturer" placeholder="Manufacturer" />
+        <input type="text" id="clubModel" placeholder="Model" />
+        <input type="text" id="clubType" placeholder="Club Type" />
+        <input type="number" id="clubLoft" placeholder="Club Loft (degrees)" />
+        <input type="number" id="clubDistance" placeholder="Club Carry (yds)" />
+        <button on:click={submit} id="addButton">Submit</button>
+        <button on:click={clearInput} id="clearButton">Clear</button>
+      </form>
+    </div>
+  
+    {#await promise then clubs}
+      <div class="grid-container">
+        <div class="golfBag">
+          <table>
+            <thead>
+              <tr>
+                <th>Manufacturer</th>
+                <th>Model</th>
+                <th>Type</th>
+                <th>Loft (degrees)</th>
+                <th>Distance (yds)</th>
+                <th>Ball Speed (mph)</th>
+                <th>Swing Speed (mph)</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody id="clubs"></tbody>
+            <tbody>
+  
+              {#each clubs as club}
+                <tr>
+                  <td>{club.manufacturer}</td>
+                  <td>{club.model}</td>
+                  <td>{club.type}</td>
+                  <td>{club.loft}</td>
+                  <td>{club.distance}</td>
+                  <td>{club.ballSpeed}</td>
+                  <td>{club.swingSpeed}</td>
+                  <button on:click={edit(club._id)}>Edit</button>
+                  <button on:click={deleteFunc(club._id)}>Delete</button>
+                </tr>
+              {/each}
+            
+            </tbody>
+          </table>
+        </div>
+      </div>
+    {/await}
+  </body>
+</div>
+  
