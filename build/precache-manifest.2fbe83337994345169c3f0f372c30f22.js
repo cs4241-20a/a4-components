20207a6fe06f3ed1e11d83b43c7f3d11c8d1ee8e
Christian Tweed <christiantweed@gmail.com>
2020-10-13 00:10:08 -0400

    Adding build for glitch

---
+++ build/precache-manifest.2fbe83337994345169c3f0f372c30f22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac5a9fd9bd3906345807f284e4810a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ea90482241345aaef67",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "6721af0911cd0f6bd054",
+    "url": "/static/css/main.a4b58b16.chunk.css"
+  },
+  {
+    "revision": "3ea90482241345aaef67",
+    "url": "/static/js/2.5cf996c8.chunk.js"
+  },
+  {
+    "revision": "54560d6c186d9b86b6d75b3cf6dcd6eb",
+    "url": "/static/js/2.5cf996c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6721af0911cd0f6bd054",
+    "url": "/static/js/main.8b42acf5.chunk.js"
+  },
+  {
+    "revision": "2cadfdd0f51b6099da8d",
+    "url": "/static/js/runtime-main.64a09c29.js"
+  }
+]);
